e33f074632c26dd34966f6d8fbe7fbf9d56fc30b
Josh Nunez <joshjnunez09@gmail.com>
2020-08-30 06:22:50 -0500

    established connection: axios && db part 2

--- client/components/app.js
+++ client/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -9,8 +10,25 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    // established axios connection to backend
+    // GET
+    axios
+      .get('/get')
+      .then((res) => console.log('Response data:', res))
+      .catch((err) => console.error(err));
+
+    // POST
+    axios
+      .post('/post', {
+        data: 'HELLO WORLD',
+      })
+      .then((res) => console.log('POSTED:', res))
+      .catch((err) => console.error(err));
+  }
+
   render() {
-    return <div>Welcome to React!</div>;
+    return <div>Hello World from Deployment</div>;
   }
 }
 
