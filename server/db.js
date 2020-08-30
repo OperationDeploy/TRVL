4a7690b398cefe1b7993d94f64bfebc3c1703592
Josh Nunez <joshjnunez09@gmail.com>
2020-08-30 06:20:11 -0500

    established connection: axios & db

---
+++ server/db.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  dialect: 'postgres',
+  pool: {
+    max: 9,
+    min: 0,
+    idle: 10000,
+  },
+});
+
+/// need to seed into db
+// const User = sequelize.define('User', {
+//   firstName: Sequelize.TEXT,
+//   lastName: Sequelize.TEXT,
+//   host: Sequelize.BOOLEAN,
+//   profile_pic: Sequelize.TEXT,
+//   email: {
+//     type: Sequelize.TEXT,
+//     unique: true,
+//   },
+// });
+
+module.exports = {
+  sequelize,
+};
