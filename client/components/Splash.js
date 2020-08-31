c506c8a0327fc5392521fb119edfeb618a2316ca
jldaggs88 <jessalyn.daggs@gmail.com>
2020-08-31 17:48:17 -0500

    Resubmission of the splash page with edits froom prdmng, webpack configuration, travis implementation, eslint plugin additions, and .jsx rule added

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -4,13 +4,11 @@ import logo from '../src/logo.png';
 // when user visits the domain they will see or logo and google login button
 // TODO: styling
 
-const Splash = () => {
-  return (
-    <div>
-      <img src={logo} alt="Logo" /> 
-      <div>"google login here"</div>
-    </div>
-  );
-};
+const Splash = () => (
+  <div>
+    <img src={logo} alt="Logo" />
+    <div>google login here</div>
+  </div>
+);
 
 export default Splash;
