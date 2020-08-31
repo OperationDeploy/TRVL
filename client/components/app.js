751de4a704627856b7c55f1d9be651b06e3690a4
Garrett Jones <garrettojones@gmail.com>
2020-08-31 15:27:19 -0500

    got all preference sliders rendering/saving state correctly on the client side

--- client/components/app.js
+++ client/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Preferences from './preferences.js';
 
 class App extends Component {
   constructor(props) {
@@ -28,7 +29,12 @@ class App extends Component {
   }
 
   render() {
-    return <div>Hello World from Deployment</div>;
+    return (
+    <div>
+      <div> Hello World from Deployment </div>
+      <Preferences />
+    </div>
+    )
   }
 }
 
