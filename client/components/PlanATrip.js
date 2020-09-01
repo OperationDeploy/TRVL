147deaf1a13b3c96a03fe3df9c9566010343d7e1
jldaggs88 <jessalyn.daggs@gmail.com>
2020-09-01 11:28:20 -0500

    Saving eslint fixes

--- client/components/PlanATrip.js
+++ client/components/PlanATrip.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 // TODO: Handle plan a trip click
 const PlanATrip = () => (
   <div>
-    <Button variant="contained" onClick={() => { alert('clicked Plan A Trip') }}>Plan A Trip</Button>
+    <Button variant="contained" onClick={() => { alert('clicked Plan A Trip'); }}>Plan A Trip</Button>
   </div>
 );
 
--- client/components/Trips.js
+++ client/components/Trips.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 // TODO: Handle trips click
 const Trips = () => (
   <div>
-    <Button variant="contained" onClick={() => { alert('clicked Trips') }}>Trips</Button>
+    <Button variant="contained" onClick={() => { alert('clicked Trips'); }}>Trips</Button>
   </div>
 );
 
