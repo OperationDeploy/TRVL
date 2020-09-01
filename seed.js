78f4fbaf1d012d325f18835c7373b9ec49027f6c
Garrett Jones <garrettojones@gmail.com>
2020-09-01 10:53:20 -0500

    Seeded the database with dummy data destinations. Also fixed the users and destinations tables for sequelize/postgres

---
+++ seed.js
@@ -0,0 +1,15 @@
+const xlsxFile = require('read-excel-file/node');
+const { Destinations } = require('./server/db');
+
+xlsxFile('./travelapp_spreadsheet.xlsx')
+  .then(async (rows) => {
+    const columnNames = rows.shift();
+    const promises = rows.map((row) => {
+      const obj = {};
+      row.forEach((cell, i) => {
+        obj[columnNames[i]] = cell;
+      });
+      return Destinations.create(obj);
+    });
+    await Promise.all(promises);
+  });
