35384b7fe48b8ff6d56d9716618ad52366a05e41
jldaggs88 <jessalyn.daggs@gmail.com>
2020-09-01 21:22:13 -0500

    Changes made to the trips feat

--- client/components/Trips.js
+++ client/components/Trips.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import Itinerary from './Itinerary';
+// import Itinerary from './Itinerary';
 
 // TODO: Handle trips click
+
 const Trips = () => (
   <div>
-    <Button variant="contained" onClick={() => { alert('clicked Trips'); }}>Trips</Button>
-    <Itinerary />
+    <Button component={Link} to="/UserTrips" variant="contained">Trips</Button>
   </div>
 );
+
+// import React from 'react';
+// import Button from '@material-ui/core/Button';
+// import Itinerary from './Itinerary';
+
+// // TODO: Handle trips click
+// const Trips = () => (
+//   <div>
+//     <Button variant="contained" onClick={() => { alert('clicked Trips'); }}>Trips</Button>
+//     <Itinerary />
+//   </div>
+// );
 
 export default Trips;
---
+++ client/components/UserTrips.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+// import Itinerary from './Itinerary';
+
+// TODO: Handle trips click
+
+const UserTrips = () => (
+  <div>
+    here are your trips
+  </div>
+);
+
+export default UserTrips;
