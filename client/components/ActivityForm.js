7ecd30a9c3eb939cad269895b8ad5832d8c205f2
jldaggs88 <jessalyn.daggs@gmail.com>
2020-09-02 13:25:45 -0500

    Add a materialui input component to add input adornment to add activity text feild

--- client/components/ActivityForm.js
+++ client/components/ActivityForm.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 
 const ActivityForm = ({ saveActivity }) => {
   const [value, setValue] = useState('');
-
   return (
     <div>
       <form
@@ -23,6 +26,20 @@ const ActivityForm = ({ saveActivity }) => {
           }}
           margin="normal"
         />
+        {/* <OutlinedInput
+          value={value}
+          variant="outlined"
+          placeholder="add activity"
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          margin="normal"
+          endAdorment={(
+            <InputAdornment position="end">
+              <IconButton aria-label="add activity" edge="end">,jh</IconButton>
+            </InputAdornment>
+          )}
+        /> */}
       </form>
     </div>
   );
