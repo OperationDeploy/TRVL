0b75eb4d4701be86841e791aa3d1d21a80a26b6c
Garrett Jones <garrettojones@gmail.com>
2020-09-02 19:59:57 -0500

    successfully rendering top 3 destinations based on user's preferences

--- client/components/SelectPlaces.js
+++ client/components/SelectPlaces.js
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const SelectPlaces = ({ currentUser, trip }) => {
-  useEffect(() => {
+  const [places, setPlaces] = useState([]);
 
+  const handleChange = (response) => {
+    setPlaces(response);
+  };
+
+  useEffect(() => {
     axios
       .post('./grabPlaces', { trip_id: trip }, (req, res) => {
-        console.log('req', req, 'res', res);
       })
       .then((response) => {
-        console.log(response);
+        handleChange(response.data);
       });
   }, []);
 
   return (
     <div>
       <header>Here are Your Places:</header>
+      <ul>
+        {places.map((dest) => (
+          <li key={dest}>
+            {dest}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
