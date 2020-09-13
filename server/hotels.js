afa49c29ed9dc784d3fcdb01d30923ad3e012060
Garrett Jones <garrettojones@gmail.com>
2020-09-13 00:48:37 -0500

    (feat) Getting data back to client, but doesn't alway have data

--- server/hotels.js
+++ server/hotels.js
@@ -3,7 +3,6 @@ const Amadeus = require('amadeus');
 const { API_KEY, API_SECRET } = require('../config');
 const { GEO_API } = require('../config');
 
-let hotels;
 const getHotelsInfo = async (req, res) => {
   const startDate = req.trip.start_date;
   const endDate = req.trip.end_date;
@@ -19,47 +18,37 @@ const getHotelsInfo = async (req, res) => {
     state = null;
   }
   const query = state ? `${city},${state},${country}` : `${city},${country}`;
-  return axios
+  await axios
     .get(
       `http://api.positionstack.com/v1/forward?access_key=${GEO_API}&query=${query}&limit=1`,
     )
     .then((response) => {
       const long = response.data.data[0].longitude;
       const lat = response.data.data[0].latitude;
-
       const amadeus = new Amadeus({
         clientId: API_KEY,
         clientSecret: API_SECRET,
       });
-
-      hotels = amadeus.shopping.hotelOffers
+      return amadeus.shopping.hotelOffers
         .get({
           latitude: lat,
           longitude: long,
-          radius: 2,
+          radius: 10,
           radiusUnit: 'MILE',
-          checkInDate: startDate,
-          checkOutDate: endDate,
+          // checkInDate: startDate,
+          // checkOutDate: endDate,
           adults: '1',
           currency: 'USD',
           bestRateOnly: true,
           sort: 'PRICE',
-        })
-        .then((results) => {
-          const result = results.data.map((hotel) => {
-            return {
-              hotelName: hotel.hotel.name,
-              price: hotel.offers[0].price.base,
-              rating: hotel.hotel.rating,
-            };
-          });
-          const arr = [result[0], result[1], result[2], result[3], result[4]];
-          return arr;
-        }).catch((err) => console.warn(err));
-        console.info(hotels);
-      res.send(hotels);
+          rating: [4, 3, 2],
+        });
     })
-    .catch((err) => console.warn(err));
+    .then((hotelList) => {
+      // console.log(hotelList);
+      res.send(hotelList.data);
+    }).catch((err) => console.warn(err));
+  // const arr = [result[0], result[1], result[2], result[3], result[4]];
 };
 
 module.exports = {
